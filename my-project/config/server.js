bf23e1f6216de6c0e508208667957bec3d82f52e
Nicolas Barthere <koff75@users.noreply.github.com>
2020-09-10 19:29:12 +0200

    Update server.js

--- my-project/config/server.js
+++ my-project/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', XXXX),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b4d84618fd31043ea427e706dd673d25'),
+      secret: env('ADMIN_JWT_SECRET', 'XXXXXXX'),
     },
   },
 });
